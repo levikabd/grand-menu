22dcf074434d9f75742b6c3b4a6e3f8dca2074f8
levikabd <levikabd@yandex.ru>
2026-01-09 18:24:00 +0700

    create_repo

---
+++ out/build/clang/CMakeFiles/grand-menu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grand-menu.dir/main.cpp.o"
+  "CMakeFiles/grand-menu.dir/main.cpp.o.d"
+  "grand-menu"
+  "grand-menu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grand-menu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
